ad32bdfb8f7a407161cb8cb8e6f154f3ddb6ee47
Tathagata16 <tathagataghosh1609@gmail.com>
2026-03-20 14:36:41 +0530

    login bug fix

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -20,15 +20,33 @@ export const useAuthStore = create((set) => ({
   },
 
   login: async (email, password) => {
-    const { data } = await api.post('/api/auth/login', { email, password });
-    localStorage.setItem('token', data.token);
-    set({ user: data.user, token: data.token });
+    try {
+      set({ loading: true }); // ✅ ADD THIS
+
+      const { data } = await api.post('/api/auth/login', { email, password });
+
+      localStorage.setItem('token', data.token);
+
+      set({ user: data.user, token: data.token, loading: false }); // ✅
+
+    } catch (err) {
+      set({ loading: false }); // ✅ important
+      throw err;
+    }
   },
 
   signup: async (username, email, password) => {
-    const { data } = await api.post('/api/auth/signup', { username, email, password });
-    localStorage.setItem('token', data.token);
-    set({ user: data.user, token: data.token });
+    try {
+      set({ loading: true });
+      const { data } = await api.post('/api/auth/signup', { username, email, password });
+      localStorage.setItem('token', data.token);
+
+      set({ user: data.user, token: data.token, loading: false });
+    }
+    catch (err) {
+      set({ loading: false });
+      throw err;
+    }
   },
 
   logout: () => {
